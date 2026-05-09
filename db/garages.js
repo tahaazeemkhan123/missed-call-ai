2ade99601b5ae3cf0d0c899d4340c05799c33edd
tahaazeemkhan123 <tahaazeemkhan123@gmail.com>
2026-05-09 13:20:40 +0400

    Update garages.js

--- db/garages.js
+++ db/garages.js
@@ -3,9 +3,9 @@
 const garages = [
   {
     id: 'garage_001',
-    name: "Auto Doctor",
+    name: "The Car Lab",
     twilioNumber: '+12497010798',
-    ownerPhone: '+971508892456',
+    ownerPhone: '+971568967912',
     hours: 'Monday to Saturday, 8am to 7pm. Closed on Sundays.',
     services: `Full car repair and maintenance including:
 - Engine diagnostics and repair
