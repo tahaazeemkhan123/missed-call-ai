5b36fd84f95bb200c70bb6f62f5ef183780b35ef
tahaazeemkhan123 <tahaazeemkhan123@gmail.com>
2026-05-11 16:39:18 +0400

    Update garages.js

--- db/garages.js
+++ db/garages.js
@@ -5,7 +5,7 @@ const garages = [
     id: 'garage_001',
     name: "Asad Auto Services",
     twilioNumber: '+12497010798',
-    ownerPhone: '+923334718444',
+    ownerPhone: 'Oscar Legacy Auto',
     hours: 'Monday to Saturday, 8am to 7pm. Closed on Sundays.',
     services: `Full car repair and maintenance including:
 - Engine diagnostics and repair
